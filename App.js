3739fd7ea31f50721922c2ed94d1f532245566b5
hackemate_ninja <nitrowitty@gmail.com>
2020-02-20 18:16:46 -0600

    utils

--- App.js
+++ App.js
@@ -1,19 +1,31 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import * as Font from "expo-font";
+import {View, Text} from "react-native";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+
+class App extends React.Component {
+  state = {
+    loading: true
+  };
+
+  async componentDidMount() {
+    await Font.loadAsync({
+      "SFPro": require("./assets/fonts/FontsFree-Net-SFProDisplay-Regular.ttf"),
+    });
+    this.setState({ loading: false });
+  }
+
+  render() {
+    if (this.state.loading) {
+      return null;
+    } else {
+      return (
+        <View>
+          <Text>Open up App.js to start working on your app!</Text>
+        </View>
+      );
+    }
+  }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
---
+++ utils/styles.js
@@ -0,0 +1,31 @@
+
+export default Styles={
+	colors: Color,
+	fonts: Font
+}
+
+const Color = {
+	bg: '#F2F2F2',
+	white: '#FFF',
+	light: '#DFE0E5',
+	lightGrey: '#7F8592',
+	dark: '#1B2749',
+	yellow: '#FFAC00',
+	purple: '#4E5AC3',
+	aqua: '#00BECE',
+	green: '#6FCF97',
+	salmon: '#FA8585',
+	red: '#FA7268',
+	blue: '#2F80ED',
+	lightBlue: '#94bcf3',
+}
+
+const Font ={
+	xl: '24px',
+	l: '17px',
+	m: '15px',
+	s: '13px',
+	medium: 500,
+	semiBold: 600,
+	bold: 700,
+}
