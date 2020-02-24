c2fce8599de34bbe2726618954d6c15b973eb983
hackemate_ninja <nitrowitty@gmail.com>
2020-02-24 08:32:51 -0600

    changes

--- App.js
+++ App.js
@@ -2,11 +2,12 @@ import React from 'react';
 import PaymentReferenceScreen from "./screens/PaymentReference/PaymentReferenceScreen";
 import PaymentThirdsScreen from "./screens/payment_thirds/PaymentThirdsScreen";
 import PaymentScreen from "./screens/payment/PaymentScreen";
+import PaymentServicesScreen from "./screens/payment_services/PaymentServicesScreen";
 
 
 const App = ()=> {
   return(
-    <PaymentScreen/>
+    <PaymentReferenceScreen/>
   );
 }
 
